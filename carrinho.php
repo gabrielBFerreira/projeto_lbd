7223c3f458676bf4e13b176be31192de3cfe25d0
Gabriel B. Ferreira <gabrielbferreira19092006@gmail.com>
2020-06-24 13:09:04 -0300

    adicionado modal do cartao de credito com validacao jquery

--- carrinho.php
+++ carrinho.php
@@ -24,12 +24,48 @@
                 echo "<input type='hidden' name='id_produto' value='".$produto['id']."'>";
                 echo "<input type='hidden' name='quantidade' value='".$produto['quantidade']."'>";
                 echo "<input type='hidden' name='valor_total' value='".$valor_total."'>";
-                echo "<input type='submit' value='Finalizar Compra'>";
+                echo "<button id='finalizar_compra'>Finalizar compra</button>";
             } else {
                 echo "Carrinho vazio";
             }
 
+            echo "<div id='modal_cartao' class='modal'>";
+                echo "<div class='modal-content'>";
+                    echo "<span class='close'>&times;</span>";
+                    echo "<p>Insira dados de seu cartão</p>";
+                    echo "<input type='text' id='campo_cartao' name='dados_cartao' required>";
+                    echo "<input type='submit' value='Confirmar'>";
+                echo "</div>";
+            echo "</div>";
+
             echo "</form>";
     }
 ?>
+
+<script>
+    $(document).ready(function(){
+        $('#campo_cartao').mask('0000 0000 0000 0000');
+    });
+
+    var modal = document.getElementById("modal_cartao");
+
+    var btn = document.getElementById("finalizar_compra");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+
 <?php require('components/bottom.php');?>
--- components/head.php
+++ components/head.php
@@ -4,4 +4,6 @@
 	<meta charset="utf-8">
 	<title>M. J. G. Instrumentos</title>
 	<link rel="stylesheet" type="text/css" href="style/main.css">
+	<script src="js/jquery-3.5.1.min.js"></script>
+	<script src="js/jquery.mask.min.js"></script>
 </head>
