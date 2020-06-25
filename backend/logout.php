b3801db5d9796ff1c4debab56b1a5c1a679a7f48
Gabriel B. Ferreira <gabrielbferreira19092006@gmail.com>
2020-06-25 11:59:01 -0300

    adicionado método de logout

---
+++ backend/logout.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    
+    unset($_SESSION["logado"]);
+    unset($_SESSION['login']);
+    unset($_SESSION['id']);
+    unset($_SESSION['nome']);
+    unset($_SESSION['tipo']);
+
+    header("Location: ../index.php");
+?>
